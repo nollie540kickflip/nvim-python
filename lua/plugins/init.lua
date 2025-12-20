d4832b5a1fa36fc79e5ccd06cead683e369b0be0
nollie540kickflip <nollie540kickflip@gmail.com>
2025-12-20 09:05:24 +0900

    プラグインを追加

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,5 +2,7 @@ return {
   { import = "plugins.python" },
   { import = "plugins.dap-python" },
   { import = "plugins.cmp" },
+  { import = "plugins.catppuccin" },
+  { import = "plugins.lualine" },
 }
 
